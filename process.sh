9597941643047bde2dc014bee00079a9e9a959e1
zieenyang <zieenyang@tencent.com>
2022-06-22 20:27:45 +0800

    fix typo error

--- process.sh
+++ process.sh
@@ -10,18 +10,18 @@ src_vocab=$workspace/src.vocab
 tgt_vocab=$workspace/tgt.vocab
 
 # bpe
-python apply_bpe.py -c $bpe_codes <$data_dir/en2cn/en2cn.train.src > $data_dir/en2cn/en2cn.train.src.bpe.cn 
-python apply_bpe.py -c $bpe_codes <$data_dir/en2cn/en2cn.train.mask > $data_dir/en2cn/en2cn.train.src.bpe.en
-python apply_bpe.py -c $bpe_codes <$data_dir/en2cn/en2cn.train.tgt > $data_dir/en2cn/en2cn.train.tgt.en
+python apply_bpe.py -c $bpe_codes <$data_dir/en2cn/en2cn.train.src > $data_dir/en2cn/en2cn.train.src.bpe.en 
+python apply_bpe.py -c $bpe_codes <$data_dir/en2cn/en2cn.train.mask > $data_dir/en2cn/en2cn.train.src.bpe.cn
+python apply_bpe.py -c $bpe_codes <$data_dir/en2cn/en2cn.train.tgt > $data_dir/en2cn/en2cn.train.tgt.cn
 
-python apply_bpe.py -c $bpe_codes <$data_dir/en2cn/en2cn.valid.src > $data_dir/en2cn/en2cn.valid.src.bpe.cn 
-python apply_bpe.py -c $bpe_codes <$data_dir/en2cn/en2cn.valid.mask > $data_dir/en2cn/en2cn.valid.src.bpe.en
-python apply_bpe.py -c $bpe_codes <$data_dir/en2cn/en2cn.valid.tgt > $data_dir/en2cn/en2cn.valid.tgt.en
+python apply_bpe.py -c $bpe_codes <$data_dir/en2cn/en2cn.valid.src > $data_dir/en2cn/en2cn.valid.src.bpe.en 
+python apply_bpe.py -c $bpe_codes <$data_dir/en2cn/en2cn.valid.mask > $data_dir/en2cn/en2cn.valid.src.bpe.cn
+python apply_bpe.py -c $bpe_codes <$data_dir/en2cn/en2cn.valid.tgt > $data_dir/en2cn/en2cn.valid.tgt.cn
 
 
 # build vocab
 touch $src_vocab
-python $codes_dir/build_vocab.py $data_dir/en2cn/en2cn.train.src.bpe.cn $data_dir/en2cn.train.src.bpe.en $src_vocab 5 
+python $codes_dir/build_vocab.py $data_dir/en2cn/en2cn.train.src.bpe.en $data_dir/en2cn.train.src.bpe.en $src_vocab 5 
 
 touch $tgt_vocab
 python $codes_dir/build_vocab.py $data_dir/en2cn/en2cn.train.tgt.bpe $tgt_vocab 5 
